53daef08ad9b1f9311dc89d91f52646f46f8549f
ouyangpan <1209899252@qq.com>
2022-11-29 20:19:30 +0800

    feat: 练习一些生命周期钩子函数

--- src/components/OldLifeCycle.jsx
+++ src/components/OldLifeCycle.jsx
@@ -1,14 +1,24 @@
 import React, { Component } from 'react'
 
 export default class OldLifeCycle extends Component {
-    componentWillReceiveProps(nextProps) {
-        // 看看该生命周期能不能改属性值，no
-        // console.log(nextProps.n = 5)
+    state = {
+        n: 1
     }
+    // componentWillReceiveProps(nextProps) {
+    //     // 看看该生命周期能不能改属性值，no
+    //     console.log(Object.isFrozen(nextProps))
+    // }
     shouldComponentUpdate(nextProps, nextState) {
         // 看看该生命周期能不能改属性值，no
         // console.log(nextProps.n = 7, nextState)
         return true
+    }
+    getSnapshotBeforeUpdate(props, state) {
+        console.log('get', props, state)
+        return null
+    }
+    componentDidUpdate() {
+        console.log('componentDidUpdate')
     }
     render() {
         return (
