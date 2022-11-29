4ea97bf6b38c858d132b58858c3f889614756e74
ouyangpan <1209899252@qq.com>
2022-11-29 22:15:57 +0800

    feat: 受控组件练习

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,13 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 function App() {
+  const [value, setValue] = useState(123)
   return (
     <div className="App">
-      
+      <input type="text" readOnly value={value} />
+      <button onClick={() => {
+        setValue(234)
+      }}>button</button>
     </div>
   );
 }
