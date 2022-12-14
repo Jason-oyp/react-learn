f8512669d9a3b64e0f32aab1ccadc2b815badb5f
ouyangpan <1209899252@qq.com>
2022-12-14 20:51:48 +0800

    feat: 研究setState的异步

--- src/components/ThreeLayout/index.jsx
+++ src/components/ThreeLayout/index.jsx
@@ -9,35 +9,58 @@ export default class ThreeLayout extends Component {
                 leftWidth: '200px',
                 rightWidth: '200px',
                 minWidth: '800px'
-            }
+            },
+            n: 1
         }
+    }
+    handleCLick() {
+        this.setState({
+            ...this.state,
+            n: this.state.n + 1
+        }, () => {
+            console.log(this.state.n)
+        })
+
+        this.setState((cur) => {
+            console.log(this.state === cur)
+            return {
+                ...this.state,
+                n: cur.n + 1
+            }
+        })
     }
     render() {
         const props = Object.assign({}, this.state.defaultProps, this.props)
         return (
-            <div className='layout' style={{
-                minWidth: props.minWidth
-            }}>
-                <div className="main">
-                    {props.children}
+            <>
+                <div>{this.state.n}</div>
+                <button onClick={() => {
+                    this.handleCLick()
+                }}>n + 1</button>
+                <div className='layout' style={{
+                    minWidth: props.minWidth
+                }}>
+                    <div className="main">
+                        {props.children}
+                    </div>
+                    <div
+                        className="aside-left"
+                        style={{
+                            width: props.leftWidth
+                        }}
+                    >
+                        {props.left}
+                    </div>
+                    <div
+                        className="aside-right"
+                        style={{
+                            width: props.rightWidth
+                        }}
+                    >
+                        {props.right}
+                    </div>
                 </div>
-                <div
-                    className="aside-left"
-                    style={{
-                        width: props.leftWidth
-                    }}
-                >
-                    {props.left}
-                </div>
-                <div
-                    className="aside-right"
-                    style={{
-                        width: props.rightWidth
-                    }}
-                >
-                    {props.right}
-                </div>
-            </div>
+            </>
         )
     }
 }
